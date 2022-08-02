a10bd9e454c3ba781791128a738168464ea560dd
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2022-08-03 01:28:12 +0530

    Updates

---
+++ precache-manifest.5f921df5aca06d65b839964d15b07999.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0069201e735fec0d24ed5e6cf798b40d",
+    "url": "/note-keeper-app/index.html"
+  },
+  {
+    "revision": "b3632c46db884d25d34f",
+    "url": "/note-keeper-app/static/js/2.84087bcb.chunk.js"
+  },
+  {
+    "revision": "f06ccb25e51f85262c54",
+    "url": "/note-keeper-app/static/js/main.55b77585.chunk.js"
+  },
+  {
+    "revision": "696e151e52b6c9af9c08",
+    "url": "/note-keeper-app/static/js/runtime-main.d85c377c.js"
+  }
+]);
